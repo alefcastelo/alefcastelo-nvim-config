6c5da0dc68873a8b7817f36e136db47cd3e4efea
Alef castelo <alefaraujocastelo@gmail.com>
2023-04-04 12:30:58 -0600

    adding setup

--- lua/alefcastelo/plugins-setup.lua
+++ lua/alefcastelo/plugins-setup.lua
@@ -65,6 +65,9 @@ return packer.startup(function(use)
   use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
   use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 
+  use("startup-nvim/startup.nvim")
+
+
   if packer_bootstrap then
     require("packer").sync()
   end
