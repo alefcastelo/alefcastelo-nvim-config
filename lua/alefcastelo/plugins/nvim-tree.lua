a64ffa8aeb235ffb2474b9b9298adbe6c1556198
Alef castelo <alefaraujocastelo@gmail.com>
2023-04-04 12:07:18 -0600

    improving configuration

--- lua/alefcastelo/plugins/nvim-tree.lua
+++ lua/alefcastelo/plugins/nvim-tree.lua
@@ -47,16 +47,11 @@ local function open_nvim_tree(data)
   end
 
   -- open the tree
-  require("nvim-tree.api").tree.open()
+  -- require("nvim-tree.api").tree.open()
 end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
-vim.api.nvim_create_autocmd("BufEnter", {
-  nested = true,
-  callback = function()
-    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-      vim.cmd "quit"
-    end
-  end
+vim.api.nvim_create_autocmd({"QuitPre"}, {
+    callback = function() vim.cmd("NvimTreeClose") end,
 })
